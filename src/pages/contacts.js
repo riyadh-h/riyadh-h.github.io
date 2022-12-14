1328f3ccab7ae13232eacdcfa7a35db0cb0f3b22
riyadh-h <riyadh-h@outlook.sa>
2022-12-14 08:21:41 +0300

    feat: add contacts.js

---
+++ src/pages/contacts.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+const ContactsPage = () => {
+    return (
+        <main>
+            <h1>Contacts</h1>
+
+            <ul>
+                <li><a href='#'>LinkedIn</a></li>
+                <li><a href='#'>GitHub</a></li>
+            </ul>
+        </main>
+    )
+}
+
+export const Head = () => <title>Contacts</title>
+
+export default ContactsPage
